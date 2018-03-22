ffb3b5a53f1cfb7d08316e316907de04fef02710
Hannah Stepanek <hmstepanek@hypothes.is>
2018-03-22 11:30:32 -0700

    Add metrics UnAuthUserGotZero & <user>GotNonZero
    UnAuthUserGotZero is, out of all the requests that were zero,
    the percentage of them where the user was None/unauthenticated.
    This will tell us if a large portion of the badge returning
    zero requests are due to unauthenticated users.
    
    <user>GotNonZero is a count or the number of requests that the
    given user got a badge count that wasn't zero. This will tell us
    if there are a small portion of users for which the badge count
    is consistently reporting non zero.

--- h/views/badge.py
+++ h/views/badge.py
@@ -9,6 +9,13 @@ from h import models, search
 from h.util.view import json_view
 
 
+def _unauth_user_got_zero_metric(user):
+    if user is None:
+        newrelic.agent.record_custom_metric('Custom/Badge/UnAuthUserGotZero', 1)
+    else:
+        newrelic.agent.record_custom_metric('Custom/Badge/UnAuthUserGotZero', 0)
+
+
 @json_view(route_name='badge')
 def badge(request):
     """Return the number of public annotations on a given page.
@@ -27,10 +34,16 @@ def badge(request):
 
     if models.Blocklist.is_blocked(request.db, uri):
         newrelic.agent.record_custom_metric('Custom/Badge/badgeCountIsZero', 1)
+        _unauth_user_got_zero_metric(request.user)
         return {'total': 0}
 
     query = {'uri': uri, 'limit': 0}
     result = search.Search(request, stats=request.stats).run(query)
 
     newrelic.agent.record_custom_metric('Custom/Badge/badgeCountIsZero', int(result.total == 0))
+    if result.total > 0:
+        user = 'None' if request.user is None else request.user.username
+        newrelic.agent.record_custom_metric('Custom/Badge/{}GotNonZero'.format(user), 1)
+    else:
+        _unauth_user_got_zero_metric(request.user)
     return {'total': result.total}
